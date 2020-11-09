79197cef2e4291ef8d994115fc3993d49f2a3476
Divya <divya.deverapally@gmail.com>
2020-11-09 10:31:24 -0500

    selection sort

---
+++ Algorithms/src/implementations/SelectionSort.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package implementations;
+
+import java.util.Scanner;
+
+/**
+ * @author Divya Deverapally
+ *Complexity - O(n^2)
+ *
+ */
+public class SelectionSort {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter array size");
+		int size = Integer.parseInt(sc.next());
+		int[] array = new int[size];
+
+		System.out.println("Enter array ");
+		for (int i = 0; i < size; i++) {
+
+			array[i] = Integer.parseInt(sc.next());
+		}
+
+		for (int element : selectionSort(array))
+			System.out.println(element);
+
+	}
+
+	static int[] selectionSort(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			int min = i;
+			for (int j = i + 1; j < array.length; j++) {
+				if (array[j] < array[min]) {
+					min = j;
+				}
+
+				int temp = array[min];
+				array[min] = array[i];
+				array[i] = temp;
+
+			}
+
+		}
+
+		return array;
+	}
+
+}
